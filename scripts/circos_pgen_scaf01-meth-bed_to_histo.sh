cc87f15d0ecaacb2743da537ac64390c8fb8af6b
Sam White <samuel.j.white@gmail.com>
2019-11-08 12:40:39 -0800

    optimize while loop for significant speed increase

--- scripts/circos_pgen_scaf01-meth-bed_to_histo.sh
+++ scripts/circos_pgen_scaf01-meth-bed_to_histo.sh
@@ -15,23 +15,12 @@ meth_bed="../feature-tracks/Pf01r.bedgraph"
 percent_meth_histo="../data/circos/plotting/pgen_scaf01_percent_meth_histo.txt"
 species=pg
 
-while IFS=$'\t' read -r line
+# Read in individual columns (this is much faster than by line)
+while IFS=$'\t' read -r scaffold start stop meth_percentage
 do
-   # Store line if percent meth is > 0
-   methylated=$(echo "${line}" | awk '$4 > 0')
-
-	 # Store percent meth value
-   zero_check=$(echo "${line}" | awk '{print $4}')
-
-	 # Pull out scaffold number and remove leading '0'
-	 scaffold_num=$(echo "${methylated}" | awk -F"[_\t]" '{print $2}' | sed 's/^0//')
-
-	 meth_position=$(echo "${methylated}" | awk '{print $2}')
-	 meth_percentage=$(echo "${methylated}" | awk '{print $4}')
-
-	 # If percent meth value is not equal (string evaluation required) to '0',
-	 # then print
-	 if [ "${zero_check}" != 0 ]
-	   then printf "%s\n" "${species}${scaffold_num} ${meth_position} ${meth_position} ${meth_percentage}"
+   if [ "${meth_percentage}" != 0 ]
+	   then
+			 scaffold_num=$(echo "${scaffold}" | awk -F"[_\t]" '{print $2}' | sed 's/^0//')
+			 printf "%s\n" "${species}${scaffold_num} ${start} ${start} ${meth_percentage}"
    fi
  done < "${meth_bed}" >> "${percent_meth_histo}"
