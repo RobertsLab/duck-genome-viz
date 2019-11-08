89f6962620710c76703abbe803ad077346ba22c9
Sam White <samuel.j.white@gmail.com>
2019-11-08 07:02:44 -0800

    initial commit

---
+++ scripts/circos_pgen_scaf01-meth-bed_to_histo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while IFS=$'\t' read -r line
+do methylated=$(echo "${line}" | awk '$4 > 0')
+   zero_check=$(echo "${line}" | awk '{print $4}')
+	 scaffold_num=$(echo "${methylated}" | awk -F"[_\t]" '{print $2}' | sed 's/^0//')
+	 meth_position=$(echo "${methylated}" | awk '{print $2}')
+	 meth_percentage=$(echo "${methylated}" | awk '{print $4}')
+	 if [ "${zero_check}" != 0 ]
+	   then printf "%s\n" "${species}${scaffold_num} ${meth_position} ${meth_position} ${meth_percentage}"
+   fi
+ done < "${meth_bed}" >> ~/programs/circos-0.69-9/data/plots/pgen_percent_meth.txt
