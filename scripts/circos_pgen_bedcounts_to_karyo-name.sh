034a76e13875f8614e5a8a38dde80834d3384cb3
sam <samwhite@uw.edu>
2019-11-12 09:40:54 -0800

    initial commit

---
+++ scripts/circos_pgen_bedcounts_to_karyo-name.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script to generate Circos histogram file from bed counts file
+
+
+# Produces output file with the following format:
+
+# label position position count
+# e.g.:
+# pg1 54 100 3.12
+
+# Variables
+bed="../"
+circos_histo="../circos/data/plotting/"
+species=pg
+
+# Read in individual columns (this is much faster than by line)
+while IFS=$'\t' read -r scaffold start stop count
+do
+  scaffold_num=$(echo "${scaffold}" | awk -F"[_\t]" '{print $2}' | sed 's/^0//')
+  printf "%s\n" "${species}${scaffold_num} ${start} ${start} ${count}"
+done < "${bed}" >> "${circos_histo}"
