f13d9adf32ff433321fe75a5199c7773082a095a
sam <samwhite@uw.edu>
2019-11-13 08:46:07 -0800

    update to parse fai more succinctly; change out fmt to include scaf name

--- scripts/circos_pgen_karyotype.sh
+++ scripts/circos_pgen_karyotype.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# Script to create karyotype file for Circos
+# Script to create karyotype file for Circos from FastA index file
 
 # Creates output file formatted like:
-# chr - pg1 1 0 89643857 chr1
+# chr - pg1 Scaffold_01 1 0 89643857 chr1
 
 # Variables
-fasta_index=../data/P_generosa/genomes/Panopea-generosa-v1.0.fa.fai
-karyo_file=../data/circos/karyotypes/karyotype.pg.txt
+fasta_index=../data/Panopea-generosa-v1.0.fa.fai
+karyo_file=../circos/data/karyotype/karyotype.pg.txt
 species=pg
 
-while IFS=$'\t' read -r line
+while IFS=$'\t' read -r name length offset linebases linewidth
 do
 	# Capture scaffold number and strip leading '0'
-	scaffold_num=$(echo "${line}" | awk -F"[_\t]" '{print $2}' | sed 's/^0//')
+	scaffold_num=$(echo "${name}" | awk -F"[_]" '{print $2}' | sed 's/^0//')
 
-	scaffold_length=$(echo "${line}" | awk '{print $2}')
-	printf "chr - %s\n" "${species}${scaffold_num} ${scaffold_num} 0 ${scaffold_length} chr${scaffold_num}"
-done < "${fasta_index}" >> "${karyo_file}"
+	printf "chr - %s\n" "${species}${scaffold_num} ${name} 0 ${length} chr${scaffold_num}"
+done < "${fasta_index}" > "${karyo_file}"
